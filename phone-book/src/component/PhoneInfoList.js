77e7066085401f9638ff499b93c7cdbfe5056c9d
YanghaKoo <33052948+YanghaKoo@users.noreply.github.com>
2018-04-23 17:44:52 +0900

    Update PhoneInfoList.js

--- phone-book/src/component/PhoneInfoList.js
+++ phone-book/src/component/PhoneInfoList.js
@@ -2,28 +2,27 @@ import React, {Component } from 'react'
 import PhoneInfo from './PhoneInfo'
 
 class PhoneInfoList extends Component{
-    static defaultProps = {
-        data : []
+    static defaultProps ={
+        data : [],
+        onRemove:  () => console.log('onRemove not defined'),
+        onUpdate:  () => console.log('onUpdate not defined')
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.data !== this.props.data;
     }
 
     render(){
-        const {data} = this.props
-        const list = data.map(
-            info => (
-                <PhoneInfo key={info.id} info={info}/>
-            )
+        console.log("render PhoneInfoList")
+        const {data, onRemove, onUpdate} = this.props
+        const list = data.map( item => 
+            <PhoneInfo info={item} key={item.id} onRemove={onRemove} onUpdate={onUpdate}/> 
         )
 
         return (
-            <div>{list}</div>
+                <div>{list}</div>
         )
-
-
-
     }
-
-
-
 
 }
 export default PhoneInfoList
