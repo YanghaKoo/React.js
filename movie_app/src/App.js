f81e9d46264c8c85845470e51390fba550a9ebba
koo <yangha93@naver.com>
2018-07-19 16:19:11 +0900

    prac

---
+++ movie_app/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
+
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    this._getMovies();
+  }
+
+  
+  componentWillMount() {
+    
+  }
+  
+
+  
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie) => {
+      //console.log(movie)
+      return (
+        <Movie
+          title={movie.title_english}
+          poster={movie.medium_cover_image}
+          key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+        />
+      );
+    });
+    return movies;
+  };
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies: movies
+    });
+  };
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")   //이 url에 json파일이 있을 경우에 아래처럼 따라하면 json으로 얻을 수 있음
+      .then(response => {     
+        return response.json();
+      })
+      .then(json => {
+        return json.data.movies;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className={this.state.movies ? "App" : "App--loading"}>
+        {this.state.movies ? this._renderMovies() : "loading"}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ movie_app/src/Movie.js
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+import LinesEllipsis from "react-lines-ellipsis";
+
+function Movie({ title, poster, genres, synopsis }) {
+  return (
+    <div className="Movie">
+      <div className="Movie_Columns">
+        <MoviePoster poster={poster} alt={title} />
+      </div>
+
+      <div className="Movie_Columns">
+        <h1>{title}</h1>
+        <div className="Movie_Genres">
+          {genres.map((genre, index) => (
+            <MoiveGenre genre={genre} key={index} />
+          ))}
+        </div>
+      </div>
+
+      <p className="Movie_Synopsis">        
+        
+        <LinesEllipsis
+          text={synopsis}
+          maxLine="3"
+          ellipsis="..."
+          trimRight
+          basedOn="letters"
+        />
+      </p>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired
+};
+
+function MoiveGenre({ genre }) {
+  return <span className="Movie_Genre">{genre} </span>;
+}
+
+MoiveGenre.propTypes = {
+  genre: PropTypes.string.isRequired
+};
+
+function MoviePoster({ poster, alt }) {
+  return <img src={poster} className="Movie_Poster" alt={alt} title={alt} />;
+}
+
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
+};
+
+export default Movie;
