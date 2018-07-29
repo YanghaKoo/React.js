62c5ab5e09726d58f22e423c37794d5e18eb9075
YanghaKoo <33052948+YanghaKoo@users.noreply.github.com>
2018-07-30 01:18:28 +0900

    Update App.js

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MyName from './MyName'
 import Counter from './Counter'
 import './App.css';
 
 
-class App extends Component {
+class App extends React.Component {
   render() {
 
  
